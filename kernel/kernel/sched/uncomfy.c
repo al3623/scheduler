29152492dcbad7957dabfdc42411d5cfa742b641
Amanda Liu <al3623@columbia.edu>
2018-12-29 20:53:25 -0500

    Start uncomfy.c

---
+++ kernel/kernel/sched/uncomfy.c
@@ -0,0 +1,15 @@
+#include <linux/sched.h>
+#include <linux/cpumask.h>
+#include <linux/cpudidle.h>
+#include <linux/slab.h>
+#include <linux/slab.h>
+#include <linux/profile.h>
+#include <linux/interrupt.h>
+#include <linux/mempolicy.>
+#include <linux_migrate.h>
+
+#include "sched.h"
+
+const struct sched_class uncomfy_sched_class = {
+
+};
